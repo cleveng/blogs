df7e77be9dbe806d99cf5a648723946499696f09
cleveng <cleveng@gmail.com>
2024-09-25 19:27:38 +0800

    perf: support deadpool-postgres

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,68 @@
 /// lib.rs
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use env_logger;
-use log::info;
+use actix_web::{middleware, web, App, HttpResponse, HttpServer};
+use deadpool_postgres::{Config, ManagerConfig, Pool, RecyclingMethod, Runtime};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+use tokio_postgres::NoTls;
 
-async fn root() -> impl Responder {
-    HttpResponse::Ok().body("Hello, Actix-web!")
+#[derive(Clone)]
+struct AppState {
+    pool: Pool,
+}
+
+/// 创建数据库连接池
+async fn get_db_pool() -> Result<Pool, Box<dyn Error>> {
+    let mut cfg = Config::new();
+    cfg.user = Some("homestead".to_string());
+    cfg.password = Some("secret".to_string());
+    cfg.host = Some("127.0.0.1".to_string());
+    cfg.port = Some(5432);
+    cfg.dbname = Some("oa".to_string());
+    cfg.manager = Some(ManagerConfig {
+        recycling_method: RecyclingMethod::Fast,
+    });
+    let pool = cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
+    Ok(pool)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Account {
+    user_id: i64,
+    role_id: i64,
+}
+
+async fn root(state: web::Data<AppState>) -> HttpResponse {
+    let client = state.pool.get().await.unwrap();
+    let row = client
+        .query_one("SELECT * from role_user", &[])
+        .await
+        .unwrap();
+
+    let account = Account {
+        user_id: row.get(0),
+        role_id: row.get(1),
+    };
+
+    HttpResponse::Ok().json(account)
 }
 
 pub async fn run() -> std::io::Result<()> {
-    // 初始化日志记录
-    std::env::set_var("RUST_LOG", "info");
-    env_logger::init();
-
     let address = "127.0.0.1:8080";
-    info!("Starting server at {}", address);
 
-    HttpServer::new(|| {
-        App::new().route("/", web::get().to(root)) // 定义根路径的 GET 请求处理
+    // 创建数据库连接池
+    let pool = get_db_pool().await;
+    if pool.is_err() {
+        panic!("Failed to create pool: {:?}", pool.unwrap_err());
+    }
+
+    let pool = pool.unwrap();
+    let state: AppState = AppState { pool: pool.clone() };
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .app_data(web::Data::new(state.clone()))
+            .route("/", web::get().to(root))
     })
     .bind(address)?
     .run()
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use blogs::run;
+use env_logger;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
     run().await
 }
